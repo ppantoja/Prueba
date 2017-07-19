67b57e3957dcb011c3c5be7ead992fb74d11f149
proyecto <penrique.potenza@gmail.com>
2017-07-19 13:52:16 -0500

    mod titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel2
+                   
                     PRUEBA DE PRUEBA
                 </div>
                 <div>
