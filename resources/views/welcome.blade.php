30de2f62427116445e66e0d67a80700c3178ad1d
Nelson Soto <nelson.soto.c05@gmail.com>
2017-07-19 11:39:37 -0430

    cambio del titulo laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel2
+                    Laravel
                 </div>
 
                 <div class="links">
