402b664c2d015eebbf0a5ebda95d691a35810d1e
Nelson Soto <nelson.soto.c05@gmail.com>
2017-07-21 10:51:35 -0430

    Agregado Iconos Fonts-Awesome Colocacion de logo de la Gobernacion de Aragua Modificacion del login

--- resources/views/layouts/zamora.blade.php
+++ resources/views/layouts/zamora.blade.php
@@ -12,17 +12,43 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{ asset('fonts/font-awesome/css/font-awesome.css')}} ">
     <style type="text/css">
         .navbar-default {
             border-color: #FF2727;
         }
-    </style>
+        .login-title-index{
+            font-size: 50px;
+            margin-top: 5%;
+        }
+
+        @media (max-width: 480px) {
+           .login-title-index{
+            font-size: 30px;
+            margin-top: 5%;
+            text-align: center;
+        }
+
+        #zamora-navbar{
+            display: none;
+        }
+
+        body{
+            margin-top: 10px;
+        }
+
+        footer{
+            font-size: 10px;
+        }
+    }
+
+</style>
 
 
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
+        <nav class="navbar navbar-default navbar-static-top" id="zamora-navbar">
             <div class="container">
                 <img class="img-responsive" src="{{ asset('images/cintillo_2017_zamora.jpg') }}">
             </div>
@@ -30,6 +56,15 @@
 
         @yield('content')
     </div>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    Desarollado por Dirección de Tecnología y Sistemas Todos los derechos para la Coordinación de Desarollo © 2017
+                </div>
+            </div>
+        </div>
+    </footer>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,67 +2,86 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<div class="row">
+						<div class="col-md-6">
+							<img src="{{ asset('images/logo_aragua_2017.png') }}" alt="" class="img-responsive">
+						</div>					
+						<div class="col-md-6  login-title-index">
+							Sisgesin
+						</div>
+					</div>
+				</div>
+				<div class="panel-body">
+					<form class="form-horizontal" method="POST" action="{{ route('login') }}">
+						{{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+						<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+							<label for="email" class="col-md-4 control-label">Usuario</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+							<div class="col-md-6">
+								<div class="input-group">
+									<input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+									<span class="input-group-addon">
+										<i class="fa fa-user" aria-hidden="true"></i>
+									</span>
+								</div>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+								@if ($errors->has('email'))
+								<span class="help-block">
+									<strong>{{ $errors->first('email') }}</strong>
+								</span>
+								@endif
+							</div>
+						</div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+						<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+							<label for="password" class="col-md-4 control-label">Contraseña</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+							<div class="col-md-6">
+								<div class="input-group">
+									<input id="password" type="password" class="form-control" name="password" required>
+									<span class="input-group-addon">
+										<i class="fa fa-lock" aria-hidden="true"></i>
+									</span>
+								</div>
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+								@if ($errors->has('password'))
+								<span class="help-block">
+									<strong>{{ $errors->first('password') }}</strong>
+								</span>
+								@endif
+							</div>
+						</div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
+						<div class="form-group">
+							<div class="col-md-6 col-md-offset-4">
+								<div class="checkbox">
+									<label data-toggle="tooltip" title="Permite recodar la contraseña, e inicia automáticamente la sesión">
+										<input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Recordar Usuario
+									</label>
+								</div>
+							</div>
+						</div>
 
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
+						<div class="form-group">
+							<div class="col-md-8 col-md-offset-4">
+								<button type="submit" class="btn btn-primary">
+									Entrar
+								</button>
 
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+								<a class="btn btn-link" href="{{ route('password.request') }}">
+									Olvidastes tu contraseña
+								</a>
+							</div>
+						</div>
+					</form>		
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
